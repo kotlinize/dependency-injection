dd2a1669796fad5160ab65f6adbaca9df00f47c0
Mike Reynolds <mikereynolds@carfax.com>
2022-07-26 17:31:10 -0500

    Finalize Gradle files

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,15 +3,40 @@ import java.net.URI
 
 plugins {
     kotlin("jvm") version "1.6.21"
-
     id("maven-publish")
     id("signing")
 }
 
-group = "com.kotlinizer"
-version = "0.0.1"
+object App {
+    object Group {
+        const val ID = "com.kotlinizer"
+        const val URL = "www.kotlinizer.com"
+    }
+    object Artifact {
+        const val NAME = "dependency-injection"
+        const val VERSION = "0.0.1"
+        const val GIT_URL = "scm:git:git://github.com/kotlinize/dependency-injection.git"
+        const val SOURCE_CODE_URL = "https://github.com/kotlinize/dependency-injection"
+        const val DESCRIPTION = "Library for Kotlin Dependency Injection"
+    }
+    object Developer {
+        const val NAME = "Michael Reynolds"
+        const val EMAIL = "mreynolds@kotlinizer.com"
+        const val USER_ID = "mreynolds25"
+    }
+    object Publish {
+        val USERNAME: String = System.getenv("NEXUS_USERNAME")
+        val PASSWORD: String = System.getenv("NEXUS_PASSWORD")
+        const val URL = "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
+    }
+    object License {
+        const val NAME = "The Apache License, Version 2.0"
+        const val URL = "http://www.apache.org/licenses/LICENSE-2.0.txt"
+    }
+}
 
-val artifactId = "dependency-injection"
+group = App.Group.ID
+version = App.Artifact.VERSION
 
 java {
     withJavadocJar()
@@ -21,46 +46,44 @@ java {
 publishing {
     publications {
         create<MavenPublication>("maven") {
-            groupId = "com.kotlinizer"
-            artifactId = artifactId
-            version = "0.0.1"
+            groupId = App.Group.ID
+            artifactId = App.Artifact.NAME
+            version = App.Artifact.VERSION
 
             from(components["java"])
 
             pom {
-                name.set(artifactId)
-                description.set("Library for Kotlin Dependency Injection")
-                url.set("www.kotlinizer.com")
+                name.set(App.Artifact.NAME)
+                description.set(App.Artifact.DESCRIPTION)
+                url.set(App.Group.URL)
                 licenses {
                     license {
-                        name.set("The Apache License, Version 2.0")
-                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                        name.set(App.License.NAME)
+                        url.set(App.License.URL)
                     }
                 }
                 developers {
                     developer {
-                        id.set("mreynolds25")
-                        name.set("Michael Reynolds")
-                        email.set("mreynolds@kotlinizer.com")
+                        id.set(App.Developer.USER_ID)
+                        name.set(App.Developer.NAME)
+                        email.set(App.Developer.EMAIL)
                     }
                 }
                 scm {
-                    connection.set("scm:git:git://github.com/kotlinize/dependency-injection.git")
-                    developerConnection.set("scm:git:git://github.com/kotlinize/dependency-injection.git")
-                    url.set("https://github.com/kotlinize/dependency-injection")
+                    connection.set(App.Artifact.GIT_URL)
+                    developerConnection.set(App.Artifact.GIT_URL)
+                    url.set(App.Artifact.SOURCE_CODE_URL)
                 }
             }
         }
         repositories {
             maven {
-
                 credentials {
-                    username = ""
-                    password = ""
+                    username = App.Publish.USERNAME
+                    password = App.Publish.PASSWORD
                 }
-
-                name = artifactId
-                url = URI.create("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+                name = App.Artifact.NAME
+                url = URI.create(App.Publish.URL)
             }
         }
     }
@@ -68,10 +91,7 @@ publishing {
 
 signing {
     signing {
-        setRequired {
-            // signing is only required if the artifacts are to be published
-            gradle.taskGraph.allTasks.any { it.equals(PublishToMavenRepository()) }
-        }
+        sign(publishing.publications)
     }
 }
 
