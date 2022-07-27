f1e978b6d982913278b1cfb57bd0f2d7d9d543c5
Mike Reynolds <mikereynolds@carfax.com>
2022-07-27 10:06:08 -0500

    Added workflow to publish to Sonatype Nexus Automatically

---
+++ dependency-injection/src/main/java/com/kotlinizer/App.java
@@ -0,0 +1,13 @@
+package com.kotlinizer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
